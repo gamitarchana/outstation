eb646fef888a7a443a4e5b79b95b1bbf98d4c400
gamitarchana <45191739+gamitarchana@users.noreply.github.com>
2019-07-29 15:57:25 +0530

    sub-path update

--- outstation/wsgi.py
+++ outstation/wsgi.py
@@ -1,21 +1,27 @@
-"""
-WSGI config for outstation project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 
-sys.path.append('/var/www/html/outstation')
-sys.path.append('/var/www/html/outstation/outstation_env')
+sys.path.append('/app/meru_os')
+sys.path.append('/app/meru_os/oscab_env')
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outstation.settings.production")
 
-application = get_wsgi_application()
+_application  = get_wsgi_application()
 
+def application(environ, start_response):
+    script_name = getattr(settings, 'X_SCRIPT_NAME', None)
+    if script_name:
+        environ['SCRIPT_NAME'] = script_name
+        path_info = environ['PATH_INFO']
+        if path_info.startswith(script_name):
+            environ['PATH_INFO'] = path_info[len(script_name):]
+
+    scheme = environ.get('HTTP_X_SCHEME', '')
+
+    if scheme:
+        environ['wsgi.url_scheme'] = scheme
+
+    return _application(environ, start_response)
